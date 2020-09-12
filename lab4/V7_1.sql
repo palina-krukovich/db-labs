125b9c4922c5c2accb70e952b49bf00e4b6cd056
polina-krukovich <polina.krukovich7@gmail.com>
2020-09-12 15:44:25 +0300

    lab4

---
+++ lab4/V7_1.sql
@@ -0,0 +1,139 @@
+﻿use AdventureWorks2012;
+go
+
+/*
+ * 1
+ * Создайте таблицу Sales.CurrencyHst, которая будет хранить 
+ * информацию об изменениях в таблице Sales.Currency.
+ * 
+ * Обязательные поля, которые должны присутствовать в таблице: 
+ * ID — первичный ключ IDENTITY(1,1); 
+ * Action — совершенное действие (insert, update или delete); 
+ * ModifiedDate — дата и время, когда была совершена операция; 
+ * SourceID — первичный ключ исходной таблицы; 
+ * UserName — имя пользователя, совершившего операцию. 
+ * 
+ * Создайте другие поля, если считаете их нужными.
+ */
+
+create table Sales.CurrencyHst (
+	ID int identity(1,1) primary key,
+	Action nvarchar(15) not null,
+	ModifiedDate datetime not null,
+	SourceID nchar(3) not null,
+	UserName nvarchar(25));
+go
+/*
+ * 2
+ * Создайте три AFTER триггера для трех операций 
+ * INSERT, UPDATE, DELETE для таблицы Sales.Currency. 
+ * Каждый триггер должен заполнять таблицу Sales.CurrencyHst 
+ * с указанием типа операции в поле Action.
+ */
+
+create trigger Sales.trgSalesCurrencyInsert
+on Sales.Currency	
+after insert as
+begin
+	set nocount on;
+	insert into Sales.CurrencyHst (
+		Action, 
+		ModifiedDate,
+		SourceID,
+		UserName)
+	select 
+		'INSERT',
+		CURRENT_TIMESTAMP,
+		CurrencyCode,
+		CURRENT_USER
+	from inserted;
+end
+go
+
+create trigger Sales.trgSalesCurrencyDelete
+on Sales.Currency	
+after delete as
+begin
+	set nocount on;
+	insert into Sales.CurrencyHst (
+		Action, 
+		ModifiedDate,
+		SourceID,
+		UserName)
+	select 
+		'DELETE',
+		CURRENT_TIMESTAMP,
+		CurrencyCode,
+		CURRENT_USER
+	from deleted;
+end
+go
+
+create trigger Sales.trgSalesCurrencyUpdate
+on Sales.Currency	
+after update as
+begin
+	set nocount on;
+	insert into Sales.CurrencyHst (
+		Action, 
+		ModifiedDate,
+		SourceID,
+		UserName)
+	select 
+		'UPDATE',
+		CURRENT_TIMESTAMP,
+		CurrencyCode,
+		CURRENT_USER
+	from inserted;
+end
+go
+
+/*
+ * 3
+ * Создайте представление VIEW, отображающее все поля таблицы Sales.Currency. 
+ * Сделайте невозможным просмотр исходного кода представления. 
+ */
+
+ create view Sales.vCurrency 
+ with encryption as
+ select * from Sales.Currency;
+ go
+
+ select definition 
+ from sys.sql_modules 
+ where object_id = object_id('Sales.vCurrency');
+ go
+
+ select * from Sales.vCurrency order by Name;
+ go
+
+ /*
+  * 4
+  * Вставьте новую строку в Sales.Currency через представление. 
+  * Обновите вставленную строку. 
+  * Удалите вставленную строку. 
+  * Убедитесь, что все три операции отображены в Sales.CurrencyHst.
+  */
+
+ insert into Sales.vCurrency (
+	CurrencyCode,
+	Name,
+	ModifiedDate)
+ values (
+	N'MYD',
+	'My Dollar',
+	CURRENT_TIMESTAMP); 
+ go
+
+ update Sales.vCurrency 
+ set Name = 'My Ruble'
+ where CurrencyCode = N'MYD';
+ go
+
+ delete from Sales.vCurrency
+ where CurrencyCode = N'MYD';
+ go 
+
+
+select * from Sales.CurrencyHst;
+go
---
+++ lab4/V7_2.sql
@@ -0,0 +1,131 @@
+﻿use AdventureWorks2012;
+go
+
+/*
+ * 1
+ * Создайте представление VIEW, отображающее данные из таблиц 
+ * Sales.Currency и Sales.CurrencyRate. 
+ * Таблица Sales.Currency должна отображать название валюты 
+ * для поля ToCurrencyCode. 
+ * Создайте уникальный кластерный индекс в представлении по полю 
+ * CurrencyRateID.
+ */
+
+ create view Sales.vCurrencyRate 
+ with schemabinding, encryption
+ as
+ select 
+	Sales.CurrencyRate.CurrencyRateID,
+	Sales.CurrencyRate.CurrencyRateDate,
+	Sales.CurrencyRate.FromCurrencyCode,
+	Sales.Currency.Name as ToCurrencyCodeName,
+	Sales.CurrencyRate.AverageRate,
+	Sales.CurrencyRate.EndOfDayRate,
+	Sales.CurrencyRate.ModifiedDate
+ from Sales.CurrencyRate
+ inner join Sales.Currency
+ on Sales.CurrencyRate.ToCurrencyCode = Sales.Currency.CurrencyCode;
+ go 
+
+ create unique clustered index 
+	idxCurrencyRateID
+ on Sales.vCurrencyRate (CurrencyRateID);
+ go
+
+ select * from Sales.vCurrencyRate;
+ go
+
+ /*
+  * 2
+  * Создайте один INSTEAD OF триггер для представления 
+  * на три операции INSERT, UPDATE, DELETE. 
+  * Триггер должен выполнять соответствующие операции 
+  * в таблицах Sales.Currency и Sales.CurrencyRate.
+  */
+
+  create trigger Sales.trg_vCurrencyRate
+  on Sales.vCurrencyRate
+  instead of insert, update, delete
+  as
+  begin
+		set nocount on;
+		if exists(select * from inserted) and exists(select * from deleted)  
+		begin
+			update scr set 
+				scr.CurrencyRateDate = i.CurrencyRateDate,
+				scr.FromCurrencyCode = i.FromCurrencyCode,
+				scr.ToCurrencyCode = sc.CurrencyCode,
+				scr.AverageRate = i.AverageRate,
+				scr.EndOfDayRate = i.EndOfDayRate,
+				scr.ModifiedDate = i.ModifiedDate
+			from Sales.CurrencyRate as scr
+			inner join inserted as i
+			on scr.CurrencyRateID = i.CurrencyRateID
+			inner join Sales.Currency as sc
+			on i.ToCurrencyCodeName = sc.Name; 
+		end
+		else if exists(select * from inserted)    
+			insert into Sales.CurrencyRate (
+				CurrencyRateDate,
+				FromCurrencyCode,
+				ToCurrencyCode,
+				AverageRate,
+				EndOfDayRate,
+				ModifiedDate)
+			select 
+				i.CurrencyRateDate,
+				i.FromCurrencyCode,
+				sc.CurrencyCode,
+				i.AverageRate,
+				i.EndOfDayRate,
+				i.ModifiedDate
+			from inserted as i
+			inner join Sales.Currency as sc
+			on i.ToCurrencyCodeName = sc.Name;
+		else if exists(select * from deleted) 
+			delete scr 
+			from Sales.CurrencyRate as scr
+			inner join deleted as d
+			on scr.CurrencyRateID = d.CurrencyRateID;
+  end
+  go
+
+/*
+ * 3
+ * Вставьте новую строку в представление, 
+ * указав новые данные для Currency и CurrencyRate 
+ * (укажите FromCurrencyCode = ‘USD’). 
+ * Триггер должен добавить новые строки в таблицы 
+ * Sales.Currency и Sales.CurrencyRate. 
+ * Обновите вставленные строки через представление. 
+ * Удалите строки.
+ */
+
+ insert into Sales.vCurrencyRate (
+	CurrencyRateDate,
+	FromCurrencyCode,
+	ToCurrencyCodeName,
+	AverageRate,
+	EndOfDayRate,
+	ModifiedDate)
+ values (
+	CURRENT_TIMESTAMP,
+	N'USD',
+	'US Dollar',
+	1.00,
+	1.00,
+	CURRENT_TIMESTAMP);	
+ go
+
+ select * from Sales.CurrencyRate
+ order by ModifiedDate desc;
+ go
+
+ update Sales.vCurrencyRate
+ set FromCurrencyCode = N'CAD'
+ where CurrencyRateID = 13534;
+ go
+
+ delete from Sales.vCurrencyRate
+ where CurrencyRateID = 13534;
+ go
