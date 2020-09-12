1ddbd9e7321f71d766e80567ee2cc0f064e7f594
polina-krukovich <polina.krukovich7@gmail.com>
2020-09-12 15:43:59 +0300

    lab3

---
+++ lab3/V7_1.sql
@@ -0,0 +1,122 @@
+﻿use AdventureWorks2012;
+go
+
+/*
+ * 1
+ * Добавьте в таблицу dbo.PersonPhone поле City типа nvarchar(30);
+ */
+
+alter table dbo.PersonPhone
+add City nvarchar(30);
+go
+
+/*
+ * 2
+ * Объявите табличную переменную с такой же структурой как dbo.PersonPhone 
+ * и заполните ее данными из dbo.PersonPhone. Поле City заполните значениями 
+ * из таблицы Person.Address поля City, а поле PostalCode значениями из 
+ * Person.Address поля PostalCode. Если поле PostalCode содержит буквы — 
+ * заполните поле значением по умолчанию
+ */
+
+declare @dboPersonPhone table (
+	BusinessEntityID int not null,
+	PhoneNumber nvarchar(25) not null,
+	PhoneNumberTypeID bigint,
+	ModifiedDate datetime not null,
+	PostalCode nvarchar(15) not null,
+	City nvarchar(30));
+
+insert into @dboPersonPhone (
+	BusinessEntityID,
+	PhoneNumber,
+	PhoneNumberTypeID,
+	ModifiedDate,
+	PostalCode,
+	City)
+select 
+	PersonPhone.BusinessEntityID,
+	PersonPhone.PhoneNumber,
+	PersonPhone.PhoneNumberTypeID,
+	PersonPhone.ModifiedDate,
+	iif(patindex('%[a-zA-Z]%', Address.PostalCode) = 0, Address.PostalCode, '0'),
+	Address.City
+ from dbo.PersonPhone
+ inner join Person.BusinessEntityAddress
+ on dbo.PersonPhone.BusinessEntityID = Person.BusinessEntityAddress.BusinessEntityID
+ inner join Person.Address
+ on Person.BusinessEntityAddress.AddressID = Person.Address.AddressID;
+
+/*
+ * 3
+ * Обновите данные в полях PostalCode и City в dbo.PersonPhone данными 
+ * из табличной переменной. Также обновите данные в поле PhoneNumber. 
+ * Добавьте код ‘1 (11)’ для тех телефонов, для которых этот код не указан
+ */
+ 
+ update dbo.PersonPhone
+ set dbo.PersonPhone.PostalCode = dboPersonPhone.PostalCode,
+	 dbo.PersonPhone.City = dboPersonPhone.City,
+	 dbo.PersonPhone.PhoneNumber = iif(patindex(
+		'%1 (11)%', dbo.PersonPhone.PhoneNumber) = 0, 
+		'1 (11)' + dbo.PersonPhone.PhoneNumber,
+		dbo.PersonPhone.PhoneNumber)	
+ from dbo.PersonPhone 
+ inner join @dboPersonPhone as dboPersonPhone
+ on dbo.PersonPhone.BusinessEntityID = dboPersonPhone.BusinessEntityID;
+ go
+
+ select * from dbo.PersonPhone;
+ go
+
+ /*
+  * 4
+  * Удалите данные из dbo.PersonPhone для сотрудников компании, 
+  * то есть где PersonType в Person.Person равен ‘EM’
+  */
+
+ select BusinessEntityID, PersonType from Person.Person;
+ go
+
+ delete from dbo.PersonPhone 
+ where exists (
+	select BusinessEntityID, PersonType
+	from Person.Person
+	where dbo.PersonPhone.BusinessEntityID = Person.Person.BusinessEntityID 
+		  and Person.Person.PersonType = 'EM'); 
+ go
+
+ select * from dbo.PersonPhone;
+ go
+
+ /*
+  * 5
+  * Удалите полe City из таблицы, удалите все созданные ограничения 
+  * и значения по умолчанию
+  */
+
+ alter table dbo.PersonPhone
+ drop column City;
+ go
+
+ select * from dbo.PersonPhone;
+ go
+
+ alter table dbo.PersonPhone
+ drop constraint CHECK_PersonPhone_PostalCode;
+ go
+
+ alter table dbo.PersonPhone
+ drop constraint DEFAULT_PersonPhone_PostalCode;
+ go
+
+ /*
+  * 6
+  * Удалите таблицу dbo.PersonPhone
+  */
+
+ drop table dbo.PersonPhone;
+ go
+
+ select * from dbo.PersonPhone;
+ go
---
+++ lab3/V7_2.sql
@@ -0,0 +1,164 @@
+﻿/* 
+ * 1
+ * Добавьте в таблицу dbo.PersonPhone поля OrdersCount INT и CardType NVARCHAR(50). 
+ * Также создайте в таблице вычисляемое поле IsSuperior, которое будет хранить 1, 
+ * если тип карты ‘SuperiorCard’ и 0 для остальных карт.
+ */
+
+ alter table dbo.PersonPhone
+ add OrdersCount int;
+ go
+
+ alter table dbo.PersonPhone
+ add CardType nvarchar(50);
+ go
+
+ alter table dbo.PersonPhone
+ add IsSuperior as iif(CardType = 'SuperiorCard', 1, 0);
+ go
+
+ select * from dbo.PersonPhone;
+ go
+
+ /*
+  * 2
+  * Создайте временную таблицу #PersonPhone, с первичным ключом по полю BusinessEntityID. 
+  * Временная таблица должна включать все поля таблицы dbo.PersonPhone за исключением поля
+  * IsSuperior.
+  */
+
+ create table #PersonPhone (
+	BusinessEntityID int not null,
+	PhoneNumber nvarchar(25) not null,
+	PhoneNumberTypeID bigint,
+	ModifiedDate datetime not null,
+	PostalCode nvarchar(15) not null,
+	OrdersCount int,
+	CardType nvarchar(50));
+ go
+
+ alter table #PersonPhone
+ add constraint PK_PersonPhone_BusinessEntityID primary key (BusinessEntityID);
+ go
+
+ /*
+  * 3
+  * Заполните временную таблицу данными из dbo.PersonPhone. 
+  * Поле CardType заполните данными из таблицы Sales.CreditCard. 
+  * Посчитайте количество заказов, оплаченных каждой картой (CreditCardID) 
+  * в таблице Sales.SalesOrderHeader и заполните этими значениями поле OrdersCount. 
+  * Подсчет количества заказов осуществите в Common Table Expression (CTE).
+  */
+
+ with CardOrders (CardID, CardOrdersCount) as (
+	select 
+		Sales.SalesOrderHeader.CreditCardID,
+		count(Sales.SalesOrderHeader.CreditCardID)
+	from Sales.SalesOrderHeader
+	group by Sales.SalesOrderHeader.CreditCardID
+ )
+ insert into #PersonPhone (
+	BusinessEntityID,
+	PhoneNumber,
+	PhoneNumberTypeID,
+	ModifiedDate,
+	PostalCode,
+	OrdersCount,
+	CardType)
+ select 
+	dbo.PersonPhone.BusinessEntityID,
+	dbo.PersonPhone.PhoneNumber,
+	dbo.PersonPhone.PhoneNumberTypeID,
+	dbo.PersonPhone.ModifiedDate,
+	dbo.PersonPhone.PostalCode,
+	CardOrders.CardOrdersCount,
+	Sales.CreditCard.CardType
+ from dbo.PersonPhone
+ left join Sales.PersonCreditCard
+ on dbo.PersonPhone.BusinessEntityID = Sales.PersonCreditCard.BusinessEntityID
+ left join Sales.CreditCard
+ on Sales.PersonCreditCard.CreditCardID = Sales.CreditCard.CreditCardID
+ left join CardOrders
+ on Sales.CreditCard.CreditCardID = CardOrders.CardID;
+ go
+
+ select * from #PersonPhone
+ order by BusinessEntityID asc;
+ go
+
+ /*
+  * 4
+  * Удалите из таблицы dbo.PersonPhone одну строку (где BusinessEntityID = 297)
+  */
+
+  select * from dbo.PersonPhone
+  where BusinessEntityID = 297;
+  go
+
+  delete from dbo.PersonPhone 
+  where BusinessEntityID = 297;
+  go
+
+  select * from dbo.PersonPhone
+  where BusinessEntityID = 297;
+  go
+
+  /*
+   * 5
+   * Напишите Merge выражение, использующее dbo.PersonPhone как target, 
+   * а временную таблицу как source. Для связи target и source используйте 
+   * BusinessEntityID. 
+   * Обновите поля OrdersCount и CardType, если запись присутствует в source 
+   * и target. Если строка присутствует во временной таблице, но не существует 
+   * в target, добавьте строку в dbo.PersonPhone. Если в dbo.PersonPhone 
+   * присутствует такая строка, которой не существует во временной таблице, 
+   * удалите строку из dbo.PersonPhone.
+   */
+
+   insert into dbo.PersonPhone (
+		BusinessEntityID,
+		PhoneNumber,
+		ModifiedDate,
+		PostalCode)
+   values (
+		987654321,
+		'987654321',
+		CURRENT_TIMESTAMP,
+		'987654321');
+   go
+
+   select * from dbo.PersonPhone
+   where BusinessEntityID = 987654321;
+   go
+
+   merge dbo.PersonPhone t 
+   using #PersonPhone s
+   on (t.BusinessEntityID = s.BusinessEntityID)
+   when matched 
+		then update set 
+			t.OrdersCount = s.OrdersCount, 
+			t.CardType = s.CardType
+   when not matched by target
+		then insert (
+			BusinessEntityID,
+			PhoneNumber,
+			PhoneNumberTypeID,
+			ModifiedDate,
+			PostalCode,
+			OrdersCount,
+			CardType) 
+		values (
+			s.BusinessEntityID,
+			s.PhoneNumber,
+			s.PhoneNumberTypeID,
+			s.ModifiedDate,
+			s.PostalCode,
+			s.OrdersCount,
+			s.CardType)
+   when not matched by source
+		then delete;
+   go
+
+   select * from dbo.PersonPhone
+   where BusinessEntityID = 297 or BusinessEntityID = 987654321;
+   go
